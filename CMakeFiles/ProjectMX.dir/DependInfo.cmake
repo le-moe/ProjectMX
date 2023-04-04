5a8e7389c70d1137c76ce74932bcaa38e1dab15d
Maurin <maurin@m1>
2023-04-04 21:53:40 +0200

    clean old docker repo

---
+++ CMakeFiles/ProjectMX.dir/DependInfo.cmake
@@ -0,0 +1,148 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/mnt/bind-root/startup_stm32f410rx.s" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/startup_stm32f410rx.s.o"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32F410Rx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "Core/Inc"
+  "Drivers/STM32F4xx_HAL_Driver/Inc"
+  "Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "Drivers/CMSIS/Include"
+  "."
+  "mnt/bind-root/PROJECT"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/mnt/bind-root/Core/Src/main.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Core/Src/main.c.o"
+  "/mnt/bind-root/Core/Src/stm32f4xx_hal_msp.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Core/Src/stm32f4xx_hal_msp.c.o"
+  "/mnt/bind-root/Core/Src/stm32f4xx_it.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Core/Src/stm32f4xx_it.c.o"
+  "/mnt/bind-root/Core/Src/system_stm32f4xx.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Core/Src/system_stm32f4xx.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc_ex.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc_ex.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_can.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_can.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cec.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cec.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_crc.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_crc.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cryp.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cryp.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cryp_ex.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cryp_ex.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dac.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dac.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dac_ex.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dac_ex.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dcmi.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dcmi.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dcmi_ex.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dcmi_ex.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dfsdm.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dfsdm.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma2d.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma2d.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dsi.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dsi.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_eth.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_eth.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_fmpi2c.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_fmpi2c.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_fmpi2c_ex.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_fmpi2c_ex.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_fmpsmbus.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_fmpsmbus.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_fmpsmbus_ex.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_fmpsmbus_ex.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_hash.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_hash.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_hash_ex.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_hash_ex.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_hcd.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_hcd.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c_ex.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c_ex.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2s.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2s.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2s_ex.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2s_ex.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_irda.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_irda.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_iwdg.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_iwdg.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_lptim.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_lptim.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_ltdc.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_ltdc.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_ltdc_ex.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_ltdc_ex.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_mmc.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_mmc.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_nand.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_nand.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_nor.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_nor.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pccard.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pccard.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pcd.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pcd.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pcd_ex.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pcd_ex.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_qspi.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_qspi.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rng.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rng.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rtc.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rtc.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rtc_ex.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rtc_ex.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_sai.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_sai.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_sai_ex.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_sai_ex.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_sd.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_sd.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_sdram.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_sdram.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_smartcard.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_smartcard.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_smbus.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_smbus.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_spdifrx.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_spdifrx.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_spi.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_spi.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_sram.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_sram.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_usart.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_usart.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_wwdg.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_wwdg.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_adc.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_adc.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_crc.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_crc.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_dac.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_dac.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_dma.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_dma.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_dma2d.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_dma2d.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_exti.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_exti.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_fmc.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_fmc.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_fmpi2c.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_fmpi2c.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_fsmc.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_fsmc.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_gpio.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_gpio.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_i2c.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_i2c.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_lptim.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_lptim.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_pwr.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_pwr.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_rcc.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_rcc.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_rng.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_rng.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_rtc.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_rtc.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_sdmmc.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_sdmmc.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_spi.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_spi.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_tim.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_tim.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_usart.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_usart.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_usb.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_usb.c.o"
+  "/mnt/bind-root/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_utils.c" "/mnt/bind-root/CMakeFiles/ProjectMX.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_utils.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "STM32F410Rx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "Core/Inc"
+  "Drivers/STM32F4xx_HAL_Driver/Inc"
+  "Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "Drivers/CMSIS/Include"
+  "."
+  "mnt/bind-root/PROJECT"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ README.md
@@ -0,0 +1,6 @@
+
+### To run a docker container with a bash shell from an image
+
+bash ```
+docker run -it --entrypoint bash toolbox
+```
